69e5b09f84a9666e499fd711d192e812fa73b0e1
Leo Brockhuis <brockhuis.leo@gmail.com>
2020-07-01 22:50:31 +0200

    add forms to problem form

--- problem_definition/problem_form.py
+++ problem_definition/problem_form.py
@@ -1,20 +1,29 @@
-import abc
+import fenics
 
-class ProblemForm(abc.ABC):
-    def __init__(self):
-        pass
 
-    def k_tilde(self):
-        pass
 
-    def mass_form_m(self):
-        pass
+class ProblemForm:
+    def __init__(self, rho, eta_m, eta_k, constitutive_relation, gamma, beta, alpha_f, alpha_m):
+        self.rho = rho
+        self.eta_m = eta_m
+        self.eta_k = eta_k
+        self.constitutive_relation = constitutive_relation
+        self.gamma = gamma
+        self.beta = beta
+        self.alpha_f = alpha_f
+        self.alpha_m = alpha_m
 
-    def stiffness_form_k(self):
-        pass
+    def mass_form_m(self, u, w):
+        return self.rho*fenics.inner(u, w)*fenics.dx
 
-    def damping_form_c(self):
-        pass
+    def damping_form_c(self, u, w):
+        return self.eta_m*self.mass_form_m(u, w) + self.eta_k*self.stiffness_form_k(u, w)
 
-    def get_weak_form(self):
-        pass
+    def stiffness_form_k(self, u, w):
+        return fenics.inner(self.constitutive_relation(u), fenics.sym(fenics.grad(w))) * fenics.dx
+
+    def get_weak_form_lhs(self, u, w):
+        k = self.stiffness_form_k
+        m = self.mass_form_m
+        c = self.damping_form_c
+        # k(u, w) +
