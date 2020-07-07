0b230427480fda483d0767e06ae0c3bcfed86915
Leo Brockhuis <brockhuis.leo@gmail.com>
2020-07-07 12:46:32 +0200

    add scipy optimize class

--- optimizer/scipy_optimizer.py
+++ optimizer/scipy_optimizer.py
@@ -5,30 +5,34 @@ from .optimizer import Optimizer
 import scipy.optimize as spo
 import numpy as np
 from fem_solver import FemSolver
+from space_definition import Spaces
 
 class ScipyOptimizer(Optimizer):
-    def __init__(self, fields: DDDbFields, initial_values: np.ndarray, fem_solver: FemSolver):
+    def __init__(self, fields: DDDbFields, initial_values: np.ndarray, fem_solver: FemSolver, spaces: Spaces):
         self.parameters = initial_values
         self.fields = fields
         self.field_to_optimize = fields.new_constitutive_relation_multiplicative_parameters
         self.fem_solver = fem_solver
+        self.spaces = spaces
+        self._results = None
 
-    def compare_displacement(self, u_1, u_2, vector_space, function_space):
-        diff = fenics.project(u_1 - u_2, V=vector_space)
-        u_norm = np.zeros(fs.dim())
+    def compare_displacement(self, fields):
+        diff = fenics.project(fields.u_new - fields.imported_displacement_field, V=self.spaces.vector_space)
+        u_norm = np.zeros(self.spaces.function_space.dim())
         for f in fenics.split(diff):
-            u_norm += fenics.project(f, V=function_space).vector()[v2d] ** 2
+            u_norm += np.power(fenics.project(f, V=self.spaces.function_space).vector()[:], 2)
         u_norm = np.sqrt(u_norm)
         return np.sqrt(np.sum(np.power(u_norm, 2)))
 
     def function_to_minimize(self, parameters):
         self.field_to_optimize.vector()[:] = parameters
         self.fem_solver.run(self.fields)
-        return self.compare_displacement()
+        return self.compare_displacement(self.fields)
 
     def run(self):
-        self._results = spo.minimize(self.function_to_minimize, self.parameters, args=(a, L, v2d, bc, u, u_old, time_generator()),
-                                      tol=1e-6, method='SLSQP', bounds=bounds, options={'maxiter': 1e6})
+        bounds = None
+        self._results = spo.minimize(self.function_to_minimize, self.parameters, tol=1e-6, method='SLSQP',
+                                     bounds=bounds, options={'maxiter': 1e6})
 
     @property
     def results(self):
--- problem_definition/dddb_fields.py
+++ problem_definition/dddb_fields.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import fenics
 import numpy as np
 from space_definition import Spaces
@@ -6,9 +8,10 @@ from .fields import Fields
 class DDDbFields(Fields):
     def __init__(self):
         super().__init__()
-        self._old_constitutive_relation_multiplicative_parameters: fenics.Function = None
-        self._new_constitutive_relation_multiplicative_parameters: fenics.Function = None
+        self._old_constitutive_relation_multiplicative_parameters: Optional[fenics.Function] = None
+        self._new_constitutive_relation_multiplicative_parameters: Optional[fenics.Function] = None
         self.tensor_space = None
+        self._imported_displacement_field: Optional[fenics.Function] = None
 
     @property
     def old_constitutive_relation_multiplicative_parameters(self) -> fenics.Function:
@@ -33,6 +36,15 @@ class DDDbFields(Fields):
     @new_constitutive_relation_multiplicative_parameters.setter
     def new_constitutive_relation_multiplicative_parameters(self, array: np.ndarray):
         self._new_constitutive_relation_multiplicative_parameters.vector()[:] = array
+
+    @property
+    def imported_displacement_field(self) -> fenics.Function:
+        if self._imported_displacement_field is None:
+            try:
+                self._imported_displacement_field = fenics.Function(self.vector_space, name='imported_displacement')
+            except TypeError:
+                print('V space was not initialized before access')
+        return self._imported_displacement_field
 
 
     def initialize(self, spaces: Spaces) -> None:
